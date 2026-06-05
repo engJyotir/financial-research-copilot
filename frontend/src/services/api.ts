3909777398b3aaa3eee6f6fa84f34cdfdbf59c69
engJyotir <jn456@njit.edu>
2026-06-05 00:40:31 -0400

    frontend deploy-2

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL ||
-    "http://127.0.0.1:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
